c4abd07e966ea15c9534ece9622e5cb8f572a11c
Ethan Knigge <etknigge03@gmail.com>
2022-03-02 19:44:23 -0800

    tutor fix

--- FIRST/wrapup/tutor.py
+++ FIRST/wrapup/tutor.py
@@ -1,18 +1,19 @@
 import random
 
-age = input('What is your age?')
+age = input('What is your age? ')
 
 correct = 0 #for keeping track of correct answers
 question = 0
-for q in range(1,11):
-    x = random.randrange(1,10)
-    y = random.randrange(1,10)
-    ans = input('what is ' + str(x) + '+' + str(y) + '? ')
-    ans = int(ans)
-    if ans == x+y:
+for q in range(1,11): #amount of loops
+    print('Problem ' + str(q))
+    x = random.randrange(1,10) #random number between 1 and 9
+    y = random.randrange(1,10) #same as x
+    ans = input('what is ' + str(x) + '+' + str(y) + '? ') #first number + second number
+    ans = int(ans) #turns into number
+    if ans == x+y: #check if correct, show below if correct
         print('Well done')
         correct = correct + 1 #keep track of correct answers
-    else:
+    else: #if wrong, show below
         print('wrong, ', x, '+', y, '=', correct)
 
 print("You got " + str(correct) + " correct. Good effort!")
